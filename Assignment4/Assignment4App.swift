c1a0425ce137a16b6904061ee54dbeb07fedfe9b
Aaron Arreola <legendofnavi1090@gmail.com>
2024-11-01 14:08:10 -0500

    Initial Commit

---
+++ Assignment4/Assignment4App.swift
@@ -0,0 +1,17 @@
+//
+//  Assignment4App.swift
+//  Assignment4
+//
+//  Created by Maggie Thomas on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct Assignment4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Assignment4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Assignment4
+//
+//  Created by Maggie Thomas on 11/1/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
