75b19123da7a9237c4581352bc4dbb2b75d31932
Aaron Arreola <legendofnavi1090@gmail.com>
2024-11-01 14:39:01 -0500

    implemented a lisst (unfinished but good framework)

--- Assignment4/PresidentListView.swift
+++ Assignment4/PresidentListView.swift
@@ -11,12 +11,17 @@ import SwiftUI
 
 struct PresidentListView: View {
     var body: some View {
-        VStack {
-            Text("Project Updated Again")
-        }
-        .padding()
-    }
-}
+        
+        let items = ["Item 1", "Item 2", "Item 3"]
+
+        //list to display Presidents
+        List(items, id: \.self) { item in
+            Text(item)
+        }//end of list
+            .listStyle(.grouped)
+        
+    }//end of some View
+}//end of struct View
 
 #Preview {
     PresidentListView()
