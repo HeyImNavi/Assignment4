192db30c826a6f8603ac528cd692deab58075f17
Aaron Arreola <legendofnavi1090@gmail.com>
2024-11-01 14:30:32 -0500

    Updating

--- Assignment4/ContentView.swift
+++ Assignment4/ContentView.swift
@@ -2,17 +2,17 @@
 //  ContentView.swift
 //  Assignment4
 //
-//  Created by Maggie Thomas on 11/1/24.
+//  Created by Aaron Arreola and Calvin Darley
+//  Due 11/12/2024
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Text("Project Updated Twice Now")
-        }
-        .padding()
+        
+        //call PresidentListView
+        PresidentListView()
     }
 }
 
--- Assignment4/PresidentListView.swift
+++ Assignment4/PresidentListView.swift
@@ -2,7 +2,22 @@
 //  PresidentListView.swift
 //  Assignment4
 //
-//  Created by Maggie Thomas on 11/1/24.
+//
+//  Created by Aaron Arreola and Calvin Darley
+//  Due 11/12/2024
 //
 
-import Foundation
+import SwiftUI
+
+struct PresidentListView: View {
+    var body: some View {
+        VStack {
+            Text("Project Updated Again")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    PresidentListView()
+}
