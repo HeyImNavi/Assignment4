57ce2768689e629e01118dd8180c4d42fdbec4b2
Aaron Arreola <legendofnavi1090@gmail.com>
2024-11-05 09:44:30 -0600

    Updated PresidentViewModel to conform with Pickers example (as directed by Kurt)

--- Assignment4/PresidentViewModel.swift
+++ Assignment4/PresidentViewModel.swift
@@ -7,32 +7,40 @@
 //
 
 import Foundation
+import Observation
 
-struct PresidentViewModel {
+@Observable
+class PresidentViewModel {
     
-    var president: PresidentPerson
+    //Declaring an array to hold values for presidents
+    var presidentArray: [PresidentPerson] = []
     
-    var name: String {
-        president.name
+    //Variables inside the presidentArray
+    var name: String = ""
+    var number: Int = 0
+    var startDate: String = ""
+    var endDate: String = ""
+    var nickname: String = ""
+    var party: String = ""
+    
+    //load contents of plist into presidentArray
+    func loadPropertyListData() {
+        
+        guard let path = Bundle.main.path(forResource: "presidents", ofType: "plist"), let xml = FileManager.default.contents(atPath: path) else {
+            fatalError("Unable to access property list presidents.plist")
+        }
+        
+        do {
+            presidentArray = try PropertyListDecoder().decode([PresidentPerson].self, from: xml)
+            
+            name = presidentArray[0].name
+            number = presidentArray[0].number
+            startDate = presidentArray[0].startDate
+            endDate = presidentArray[0].endDate
+            nickname = presidentArray[0].nickname
+            party = presidentArray[0].party
+        } catch {
+            fatalError("Unable to decode property list presidents.plist")
+        }
     }
-    
-    var number: Int {
-        president.number
-    }
-    
-    var startDate: String {
-        president.startDate
-    }
-    
-    var endDate: String {
-        president.endDate
-    }
-    
-    var nickname: String {
-        president.nickname
-    }
-    
-    var party: String {
-        president.party
-    }
-}//end of struct
+}
