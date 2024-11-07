1fbab320a466c34ba0e13a460059325f69f17250
Aaron Arreola <legendofnavi1090@gmail.com>
2024-11-07 13:11:50 -0600

    Formatting and details

--- Assignment4/Views/PresidentDetailView.swift
+++ Assignment4/Views/PresidentDetailView.swift
@@ -13,11 +13,35 @@ struct PresidentDetailView: View {
     var president: PresidentViewModel
     
     var body: some View {
+        
         VStack(spacing: 16) {
+            
+            //Name of President
             Text(president.name)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .multilineTextAlignment(.center)
+            
+            //What number the President is
+            Text("\(president.number) President of the United States") //need to implement number formatter
+                .bold()
+            
+            //Start and End Date of the President
+            Text("(\(president.startDate) to \(president.endDate))")
+            
+            //Presidential Seal Image
+            Image(.seal)
+            
+            //Nickname of the President
+            Text("Nickname")
+                .bold()
+            Text("\(president.nickname)")
+            
+            //Political Party of the President
+            Text("Political Party")
+                .bold()
+            Text(president.party)
+            
             Spacer()
         }//end of VStack
     }//end of some View
